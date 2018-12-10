7626cead077a91ea75b6fc0b6a3518bc717a1685
Unknown <Sm7684@nyu.edu>
2018-12-10 13:56:08 -0500

    cam movement + new sounds
    still aint that great

---
+++ Assets/CamRotation.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotation : MonoBehaviour
+{
+
+	private float desiredRot;
+	public float rotSpeed = 250;
+	public float damping = 10;
+
+	private Quaternion originalRotationValue;
+	private float rotationResetSpeed = 10f;
+
+	private bool firstCoRoutineClick; 
+	private bool secondCoRoutineClick; 
+
+	
+	void Start()
+	{
+		originalRotationValue = transform.rotation;
+		desiredRot = transform.eulerAngles.z;
+
+	}
+	
+	private IEnumerator CamRotate()
+	{
+	
+		firstCoRoutineClick = false; 
+		
+		while (enabled)
+		{
+
+			yield return new WaitForSeconds(5);
+			firstCoRoutineClick = true;
+
+			if (firstCoRoutineClick)
+			{
+				if (Input.GetMouseButton(0))
+				{
+					if (Input.mousePosition.x > Screen.width / 2) desiredRot -= rotSpeed * Time.deltaTime;
+					else desiredRot += rotSpeed * Time.deltaTime;
+				}
+
+				var desiredRotQ = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, desiredRot);
+				transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotQ, Time.deltaTime * damping);
+			} 
+
+			yield return new WaitForSeconds(10);
+			transform.rotation = Quaternion.Slerp(transform.rotation, originalRotationValue, Time.time * rotationResetSpeed);
+			yield return null;
+		//	firstCoRoutineClick = false; 
+		}
+	}
+
+	private void Update()
+
+	{
+		StartCoroutine(CamRotate());
+
+	}
+}  
