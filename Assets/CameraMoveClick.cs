320f87079c5d293a317db951abc0732547689837
Unknown <Sm7684@nyu.edu>
2018-12-11 01:33:45 -0500

    added gradients and camera
    --

---
+++ Assets/CameraMoveClick.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Pixelplacement.TweenSystem;
+using UnityEngine;
+
+public class CameraMoveClick : MonoBehaviour
+{
+	public MousePressed _MousePressed;
+	public Vector3 _Position; 
+
+	
+	// Use this for initialization
+	void Start () {
+		_MousePressed._mousePressed = false;
+		_MousePressed._mousePressedTwice = false; 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		//Vector3 _currentPosition = _Position; 
+		
+		if (_MousePressed._mousePressed)
+		{
+			transform.position = Vector3.Lerp (transform.position, new Vector3(0,16.7f, -10), 3f * Time.deltaTime);
+
+		}
+
+		if (_MousePressed._mousePressedTwice)
+		{
+			transform.position = Vector3.Lerp (transform.position, new Vector3(10,16.7f, -10), 2f * Time.deltaTime);
+
+		}
+	}
+}
---
+++ Assets/MousePressed.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePressed : MonoBehaviour {
+
+
+	public bool _mousePressed;
+	public bool _mousePressedTwice; 
+
+	//public Vector3 screenPoint; 
+	//public float _speed;
+
+	//public Vector3 _currentPosition;
+
+	//public Vector3 _newPosition; 
+	// Use this for initialization
+	void Start ()
+	{
+		_mousePressed = false;
+		_mousePressedTwice = false; 
+	}
+	
+	// Update is called once per frame
+	void OnMouseOver() {
+		
+		if (Input.GetMouseButtonDown(0))
+		{
+			_mousePressed = true; 		
+			
+			//screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+			//Camera.main.transform.position = Vector3.Lerp (transform.position, new Vector3(0,16.5f, -10), 10f);
+			if (Input.GetMouseButtonDown(1))
+			{
+				_mousePressedTwice = true;
+				Debug.Log("PRESSED 2");
+			} 
+
+		}
+
+
+	}
+}
