e74f5d905993f7fee738aa0bd16b81c187c9539f
Unknown <Sm7684@nyu.edu>
2019-01-22 15:04:26 -0500

    New Scene!!

---
+++ Assets/InstantiateSoundMakingObjects.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateSoundMakingObjects : MonoBehaviour
+{
+
+	public GameObject soundMakingObject; 
+	public GameObject soundMakingObjectClone; 
+		
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown()
+	{
+		
+		
+	}
+}
---
+++ Assets/OnTriggerChangeColor.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerChangeColor : MonoBehaviour
+{
+	private SpriteRenderer SR; 
+	
+	void Awake ()
+	{
+
+		SR = GetComponent<SpriteRenderer>(); 
+		SR.color = Color.black;
+
+				
+	}
+
+
+	IEnumerator WaitALil()
+	{
+		yield return new WaitForSeconds(5f);
+		
+	}
+	
+	void OnTriggerEnter2D (Collider2D col) {
+
+		/*
+		if (col.gameObject.CompareTag("Ball") && Camera.main.WorldToScreenPoint(transform.position).x > Screen.width / 2)
+		{
+		
+			SR.color = Color.black;
+			
+		}
+
+		else
+		{
+			
+			SR.color = Color.black;
+
+		}
+		*/
+
+		StartCoroutine(WaitALil()); 
+		
+		if (col.gameObject.CompareTag("Ball")) 
+		{
+		
+			SR.color = Random.ColorHSV();
+			
+		}
+
+		else
+		{
+			
+			SR.color = Color.black;
+
+		}
+		
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+
+			SR.color = Color.black;
+			
+	}
+}
