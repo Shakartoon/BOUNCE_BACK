879c0f07551cd67a766aa9184208cd0bd2840b17
Unknown <Sm7684@nyu.edu>
2019-02-19 00:38:29 -0500

    patternz
    **

---
+++ Assets/LineRendererStuff.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererStuff : MonoBehaviour
+{
+	private LineRenderer myLR;
+	public float widthx; 
+	public float widthy;
+
+	public Color c1 = Color.yellow;
+	public Color c2 = Color.red;
+	public int lengthOfLineRenderer = 20;
+	
+	
+	void Start ()
+	{
+
+		myLR = GetComponent<LineRenderer>();
+		myLR.material = new Material(Shader.Find("Sprites/Default"));
+		myLR.widthMultiplier = 0.2f;
+		myLR.positionCount = lengthOfLineRenderer;
+
+		// A simple 2 color gradient with a fixed alpha of 1.0f.
+		float alpha = 1.0f;
+		Gradient gradient = new Gradient();
+		gradient.SetKeys(
+			new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+			new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+		);
+		myLR.colorGradient = gradient;
+
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		myLR.SetWidth(widthx++ / 5f, widthy++ / 15f);
+		
+		var t = Time.time;
+		for (int i = 0; i < lengthOfLineRenderer; i++)
+		{
+			myLR.SetPosition(i, new Vector3(i * 0.1f, Mathf.Sin(i + t), 0.0f));
+		}
+		
+		transform.Translate(2, 2, 0);
+	}
+}
---
+++ Assets/PatternScales.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternScales : MonoBehaviour
+{
+
+	public bool scaleIt;
+	public bool rotateIt; 
+	
+	public float speed = 2f; 
+	
+	private Vector3 initialScale;
+	private Vector3 newScale; 
+	
+	void Start ()
+	{
+		
+		newScale = new Vector3(3, 3, 0);
+		initialScale = new Vector3(0.5f - 1, 0.5f - 1, 0);
+
+	}
+	
+	void Update ()
+	{
+
+		MakeItBigger();
+		RotateIt(); 
+
+	}
+		
+	void RotateIt()
+	{
+		if (rotateIt)
+		{
+			
+			float rude = Mathf.Deg2Rad;
+			//transform.localEulerAngles = new Vector3(0, 0, rude * 25f) * Time.deltaTime * speed; 
+			//transform.RotateAround(Vector3.left, Vector3.down * 4f, 20 * Time.deltaTime * 20f);
+			transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+
+		}
+		
+	}
+
+	void MakeItBigger()
+	{
+		
+		if (scaleIt)
+		{
+			transform.localScale = Vector3.Lerp(transform.localScale, newScale, speed * Time.deltaTime);
+		
+			if (transform.localScale.x >= 2f)
+			{
+				transform.localScale = Vector3.Lerp(transform.localScale, initialScale, speed * Time.deltaTime);
+				transform.localScale = Vector3.zero;
+			}
+		}
+		
+	}
+
+}
