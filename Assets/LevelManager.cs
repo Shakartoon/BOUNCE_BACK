0712a187ffc058bf08840bb4a2dba94d674dcdd1
Unknown <Sm7684@nyu.edu>
2019-03-29 01:09:49 -0400

    push

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening; 
 
 public class LevelManager : MonoBehaviour
 {
-
+	
+	public ONLYClickAndDrag _clickanddragscript; 
 	public GameObject firstBar;
 	public GameObject[] barArray;
+	public GameObject lineParent; 
 
-	private bool _OnMouseDown; 
+	private bool _OnMouseDown;
+	private float time; 
 	
-	void Start () {
-		
+	void Start ()
+	{
+		_clickanddragscript._OnMouseUp = false;
+		lineParent.SetActive(false);
 	}
 	
 	void Update () {
@@ -20,9 +26,12 @@ public class LevelManager : MonoBehaviour
 		{
 			Debug.Log("HasChanged");
 
-			if (_OnMouseDown)
+			if (_clickanddragscript._OnMouseUp)
 			{
-				firstBar.transform.position = Vector3.zero;
+				Debug.Log("Mose"); 
+				firstBar.transform.DOMove(new Vector3(0, 10.9f), 2, false); 
+				GetMyChildren(); 
+
 			}
 			
 			// Wait 1 second 
@@ -33,9 +42,22 @@ public class LevelManager : MonoBehaviour
 		
 	}
 
-	void OnMouseDown()
+	void GetMyChildren()
 	{
-		_OnMouseDown = true; 
+		foreach (Transform child in lineParent.transform)
+		{
+
+			for (int i = 0; i < barArray.Length; i++)
+			{
+			
+				lineParent.SetActive(true);
+
+			}
+			
+			
+		}
+
+		
 	}
 	
 }
--- Assets/ONLYClickAndDrag.cs
+++ Assets/ONLYClickAndDrag.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ONLYClickAndDrag : MonoBehaviour {
-	
-	
+public class ONLYClickAndDrag : MonoBehaviour
+{
+
+	//Make it public but not viewable in inspector
+	public bool _OnMouseDown;
+	public bool _OnMouseDrag;
+	public bool _OnMouseUp; 
 	private Vector3 offset;
 
 	void OnMouseDown()
@@ -20,5 +24,10 @@ public class ONLYClickAndDrag : MonoBehaviour {
 		Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
 		transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
 		
+	}
+
+	void OnMouseUp()
+	{
+		_OnMouseUp = true; 
 	}
 }
