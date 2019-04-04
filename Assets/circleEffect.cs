beb0af4a35ffc65b2bdb53afa82f2cb00d115c9f
Unknown <Sm7684@nyu.edu>
2019-04-04 00:48:43 -0400

    m

--- Assets/circleEffect.cs
+++ Assets/circleEffect.cs
@@ -1,44 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening; 
 
 public class circleEffect : MonoBehaviour
 {
 
+	//How to reloop outline 
+	
+	
 	[SerializeField] float expandTime;
 	[SerializeField] private GameObject loadMe;
 	private GameObject instantedThing;
 	private bool isDestroyed;
 	private bool instantied;
+	public bool inner; 
 
-
+	//outline 
+	public bool outer;
+	private bool Collision; 
+	public GameObject outline1, outline2, outline3;
 
 	void Start()
 	{
-
+		outline1.SetActive(false);
+		outline2.SetActive(false);
+		outline3.SetActive(false);
+		
 	}
-
+	
 	void InstantiateMe()
 	{
 		
 		Instantiate(loadMe, Vector3.zero, transform.rotation);
-
 		
 	}
-	
-	
+
+	public void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("SoundMakingObject"))
+		{
+			Collision = true;
+			Debug.Log("Hi"); 
+		}
+	}
 
 	void Update()
 	{
 
-		float scale = 1f;
-		//transform.localScale = new Vector3(scale, scale, 0) * Time.time * expandTime;
-
-
-		if (instantied == false)
+		if (outer)
 		{
-			InstantiateMe(); 
-			instantied = true;
+			if (Collision)
+			{
+				outline1.SetActive(true);
+				outline1.transform.DOScale(3f, 3f);
+				Destroy(outline1, 2);
+			}
+			
+		}
+		
+		if (inner)
+		{
+			float scale = 1f;
+			//transform.localScale = new Vector3(scale, scale, 0) * Time.time * expandTime;
+
+
+			if (instantied == false)
+			{
+				InstantiateMe();
+				instantied = true;
+			}
 		}
 
 	}
