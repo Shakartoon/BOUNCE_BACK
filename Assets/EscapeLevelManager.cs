344a386cdcc2af5dfbea7b001496c413873133e2
Unknown <Sm7684@nyu.edu>
2019-04-14 23:06:47 -0400

    textures
    --

--- Assets/EscapeLevelManager.cs
+++ Assets/EscapeLevelManager.cs
@@ -6,20 +6,25 @@ using DG.Tweening;
 public class EscapeLevelManager : MonoBehaviour
 {
 
-	public GameObject rect1, rect2, rect3, rect4;
-	public Vector3 V31, V32, V33, V34;
-	
-	void Start () {
+	public GameObject player;
+	private Rigidbody2D playerRB;
+	private float timer = 10f;
+	private float time; 
 		
+	void Start ()
+	{
+
+		playerRB = player.GetComponent<Rigidbody2D>(); 
+		playerRB.isKinematic = true; 
+
 	}
 	
-	public void Update() { 
-		
-		/* 
-		rect1.transform.DOMove(V31, 1);
-		rect2.transform.DOMove(V32, 1);
-		rect3.transform.DOMove(V33, 1);
-		rect4.transform.DOMove(V34, 1);
-		*/ 
+	void Update()
+	{
+		time += Time.deltaTime; 
+		if(time >= timer)
+		{
+			playerRB.isKinematic = false;
+		}
 	}
 }
