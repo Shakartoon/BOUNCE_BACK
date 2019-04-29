91b9af771183560694a1b635cb5b4a498bd97015
Unknown <Sm7684@nyu.edu>
2019-04-29 01:57:31 -0400

    organized and animated

---
+++ Assets/_Codes/ImmigrationLevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImmigrationLevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//if collision count is a certain number 
+		// make snake move more 
+		// move camera 
+		// move objects to camera 
+		
+	}
+}
