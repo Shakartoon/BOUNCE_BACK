9b15e23fc63befd073723aed85d7e08f3aedc1a5
Unknown <Sm7684@nyu.edu>
2019-04-30 00:16:26 -0400

    sounds and animation
    --

--- Assets/_Codes/AnimationManager_Immigration.cs
+++ Assets/_Codes/AnimationManager_Immigration.cs
@@ -1,26 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening; 
 
 public class AnimationManager_Immigration : MonoBehaviour
 {
 
 	public GameObject player2;
-	private Animator player2Animator; 
+	//private Animator player2Animator; 
 	
 	void Start ()
 	{
 
-		player2Animator = player2.GetComponent<Animator>(); 
+		//player2Animator = player2.GetComponent<Animator>(); 
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
+		player2.transform.position = Vector3.Lerp(new Vector3(24.1f, -10f, -15.8f), new Vector3(24f, -11f, 10.1f), 2f);  
+			
+
+		//player2.transform.DOMove(new Vector3(24f, -11f, 10.1f), 3f);
+		/* 
 		if (player2Animator.GetCurrentAnimatorStateInfo(0).IsName("Finished"))
 		{
 			player2Animator.enabled = false; 
 		}
-		
+		*/
+
 	}
 }
