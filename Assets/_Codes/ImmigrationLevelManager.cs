cc0de29e370f5e53dc9cfc7e6eaaf34c3e6607d6
Unknown <Sm7684@nyu.edu>
2019-05-03 17:29:41 -0400

    papiros
    00

--- Assets/_Codes/ImmigrationLevelManager.cs
+++ Assets/_Codes/ImmigrationLevelManager.cs
@@ -4,19 +4,47 @@ using UnityEngine;
 
 public class ImmigrationLevelManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject player;
+	private Rigidbody2D playerRigidBody;
+	private SpriteRenderer playerSpriteRenderer; 
+
+	public float timer = 1;
+	private float time;
+
+	public bool PlayerIsOnTheRight; 
+
+	void Start ()
+	{
+		playerRigidBody = player.GetComponent<Rigidbody2D>(); 
+		playerSpriteRenderer = player.GetComponent<SpriteRenderer>();
 		
+		playerRigidBody.isKinematic = true;
+		//playerSpriteRenderer.color = new Color (1, 0, 0, 0); 
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		//Any way to make this more efficient? 
+		time += Time.deltaTime;
+		if (time >= timer)
+		{
+			playerRigidBody.isKinematic = false; 
+			//playerSpriteRenderer.color = new Color (1, 1, 1, 1); 
+
+		}
+		
+		if (player.transform.position.x >= 24f)
+		{
+			PlayerIsOnTheRight = true; 
+		}
+
 		
 		//Start by placing tiles 
 		//if collision count is a certain number 
 		// make snake move more 
 		// move camera 
 		// move objects to camera 
-		
+
 	}
 }
