a67c87a3fcf0761b0becd9bd073d1527a4beb00c
Unknown <Sm7684@nyu.edu>
2019-05-04 06:22:35 -0400

    moving noise
    --

---
+++ Assets/NoiseMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseMover : MonoBehaviour {
+
+    public float timer; 
+    float time; 
+
+	void Start () {
+		
+	}
+	
+	void Update ()
+	{
+
+		time += Time.deltaTime; 
+		if (time >= timer)
+		{
+			transform.localPosition = new Vector2(Random.Range(-4.3f, 2f), Random.Range(2.3f, 5.6f));
+			time = 0; 
+		} 
+
+	}
+}
