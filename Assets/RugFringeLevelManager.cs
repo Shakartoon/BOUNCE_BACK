4e4565b974e41d7dd5332624e2ecb1e45c298c06
Unknown <Sm7684@nyu.edu>
2019-05-10 15:03:10 -0400

    rf g

---
+++ Assets/RugFringeLevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RugFringeLevelManager : MonoBehaviour
+{
+
+	public GameObject tileObj1, tileObj2;
+	public float TimeToMoveTitleObjs;
+	private float time; 
+	
+	void Start ()
+	{
+		tileObj1.GetComponent<moveAcrossScreen>().enabled = false; 
+		tileObj2.GetComponent<moveAcrossScreen>().enabled = false; 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		time += Time.deltaTime;
+		if (time >= TimeToMoveTitleObjs)
+		{
+			tileObj1.GetComponent<moveAcrossScreen>().enabled = true; 
+			tileObj2.GetComponent<moveAcrossScreen>().enabled = true; 
+		}
+
+	}
+}
