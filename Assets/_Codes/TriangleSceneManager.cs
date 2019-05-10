e5045863b3cf414f9b9c4e50bdbf6ad1ce9d9ece
Unknown <Sm7684@nyu.edu>
2019-05-10 19:45:42 -0400

    dope
    --

--- Assets/_Codes/TriangleSceneManager.cs
+++ Assets/_Codes/TriangleSceneManager.cs
@@ -1,23 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening; 
 
 public class TriangleSceneManager : MonoBehaviour
 {
-	private float timer = 5;
+	public GameObject row1, row2; 
+	public float timeToMoveCamera; 
+	public float timeToLoadNextScene = 45;
 	private float time; 
 	void Start () {
 		
+		row1.SetActive(true);
+		row2.SetActive(true);
 	}
 
 	void Update()
 	{
 		
 		time += Time.deltaTime;
-		
-		if (time >= timer)
+
+		if (time >= timeToMoveCamera)
 		{
-			NewLevelManagement.lvlManagement.isConditionReached = true; 
+			Camera.main.transform.DOMove(new Vector3(Camera.main.transform.position.x, -616f, Camera.main.transform.position.z), 2); 
+
+		}
+
+		if (time >= 30)
+		{
+			row1.SetActive(false);
+			row2.SetActive(false);
+			
+		}
+		
+		if (time >= timeToLoadNextScene)
+		{
+			//NewLevelManagement.lvlManagement.isConditionReached = true; 
+			NewLevelManagement.lvlManagement.LoadNextLevel();
+
 		}
 
 	} 
