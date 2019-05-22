0a4068b98d8b0604653f1dad06350121f19ff8a6
Unknown <Sm7684@nyu.edu>
2019-05-22 02:23:43 -0400

    push

---
+++ Assets/Immigration2LevelManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening; 
+
+public class Immigration2LevelManager : MonoBehaviour
+{
+
+	public moveAcrossScreen camMove;
+	public float timer = 5f;
+	public float time;  
+
+	public GameObject[] things; 
+	private SpriteRenderer[] sprites; 
+	
+	void Start ()
+	{
+
+		camMove.enabled = false; 
+		sprites = new SpriteRenderer[things.Length];
+		
+		for (int i = 0; i < sprites.Length; i++)
+		{
+			sprites[i] = things[i].GetComponent<SpriteRenderer>(); 
+		}
+
+	}
+	
+	void Update ()
+	{
+
+		time += Time.deltaTime;
+		if (time >= timer)
+		{
+			camMove.enabled = true; 
+			
+			for (int i = 0; i < sprites.Length; i++)
+			{
+				sprites[i].DOColor(new Color(1, 1, 1, 0), 4f); 
+			}
+		}
+
+	}
+}
