f4b8b4a5eff9b20dbf53ae3e88270c2fcf4575da
Unknown <Sm7684@nyu.edu>
2019-05-21 21:54:34 -0400

    hi

---
+++ Assets/NoiseScale.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseScale : MonoBehaviour {
+
+    public float timer; 
+    private float time; 
+    
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		time += Time.deltaTime;
+		if (time >= timer)
+		{
+			transform.localScale = new Vector3(Random.Range(0, 7), Random.Range(0, 7), 0); 
+			time -= Time.deltaTime; 
+		}
+		
+	}
+}
