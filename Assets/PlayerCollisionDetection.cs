d8f96989e659beeeb98e0ed763e5010d2efda369
Unknown <Sm7684@nyu.edu>
2019-05-23 04:14:40 -0400

    hi

---
+++ Assets/PlayerCollisionDetection.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionDetection : MonoBehaviour
+{
+
+	public GameObject border; 
+	private Animator borderAnim;
+	private bool collisionYes;
+	private float timer = 1f, time;
+	public float minusVal = 0.3f;
+	
+	void Start ()
+	{
+
+		borderAnim = border.GetComponent<Animator>();
+		borderAnim.enabled = false;
+	}
+
+	void Update()
+	{
+		
+
+		if (collisionYes)
+		{
+			time += Time.deltaTime; 
+
+			if (null != borderAnim)
+			{
+				borderAnim.enabled = true; 
+			}
+			
+		else if (!collisionYes)
+			{
+				borderAnim.enabled = false; 
+
+			}
+
+		}
+	}
+	
+	
+	void OnCollisionEnter2D (Collision2D col) {
+
+		if (col.gameObject.CompareTag("PSObject"))
+		{
+			collisionYes = true;
+			//border.transform.localScale -= new Vector3(transform.localScale.x - minusVal, transform.localScale.y - minusVal, transform.localScale.z);
+
+		}
+	
+	}
+
+}
--- Assets/QuarrelLevelManager.cs
+++ Assets/QuarrelLevelManager.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class QuarrelLevelManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public GameObject player, pink, blue;
+	private float time, timer = 2f; 
+	
+	void Start ()
+	{
+
+		player.GetComponent<Rigidbody2D>().isKinematic = true; 
+		pink.SetActive(false);
+		blue.SetActive(false);
+
+
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		time += Time.deltaTime; 
+		if (time >= timer)
+		{
+			player.GetComponent<Rigidbody2D>().isKinematic = false; 
+
+		}
+
+		if (time >= timer + 1f)
+		{
+			pink.SetActive(true);
+			blue.SetActive(true);
+		}
 	}
 }
